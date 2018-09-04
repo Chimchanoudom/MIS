b45fb1ee1da0e8969faf9e78102e48b0b3cca5d8
Chimchanoudom <chanoudomchim@gmail.com>
2018-09-04 11:37:32 +0700

    g

--- MIS/GuestHouse/GuestHouse/Dom_SqlClass.cs
+++ MIS/GuestHouse/GuestHouse/Dom_SqlClass.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace GuestHouse
 {
-    static class Dom_SqlClass
+    class Dom_SqlClass:UserLoginDetail
     {
+      static  SqlCommand SC = new SqlCommand();
+       static SqlCommandBuilder SCB = new SqlCommandBuilder();
+        
         public static void AddValue()
         {
 
         }
-        public static void retriveData(String TableName)
+        public static void retriveData(String TableName,DataGridView Data)
         {
+            try
+            {
+                dataCon.Con.Open();
+                SC = new SqlCommand("Select * from "+TableName+";", dataCon.Con);
 
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
         public static void Edite()
         {
